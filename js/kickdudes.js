93f1702533de4d81309ea3ca2d97182550f337bb
Champ <twinsynergy@Twinsynergys-MacBook-Pro.local>
2015-05-06 16:14:49 +0700

    change js menu slide mobile

--- js/kickdudes.js
+++ js/kickdudes.js
@@ -3,8 +3,16 @@ $(function() {
   // document.querySelector( "#hamburger" ).addEventListener( "click", function() {
   //   this.classList.toggle( "active" );
   // });
-  $('#hamburger').sidr({
-    name: 'sidr-existing-content',
-    source: '#sidr-content'
-  });
+
+  $('#main_navbar').clone().attr("id","nav_mobile").appendTo('nav.navbar-fixed-top .container').addClass('cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left');
+
+  var menuLeft = document.getElementById( 'nav_mobile' ),
+      showLeft = document.getElementById( 'hamburger' )
+
+  showLeft.onclick = function() {
+  	classie.toggle( this, 'active' );
+  	classie.toggle( menuLeft, 'cbp-spmenu-open' );
+  };
+
+
 });
