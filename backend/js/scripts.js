9c52377a71ba5b2b1e4f88dc8404574480969dbd
Champ <twinsynergy@Twinsynergys-MacBook-Pro.local>
2015-05-13 16:47:41 +0700

    field page

--- backend/js/scripts.js
+++ backend/js/scripts.js
@@ -34,7 +34,27 @@ $(function(){
   $('[data-toggle="tooltip"]').tooltip();
 
   /*Form*/
-  // $('tbody.rowlink').rowlink();
+  var MaxFields = 10;
+  var Wrapper = $('#fields-form .time-wraper');
+  var AddButton = $("#fields-form .insert-time");
+  var RemoveButton = $('#fields-form .remove-time');
+  var Row = '<div class="row time-row time-minus-row"><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" data-mask="99:99" placeholder="เวลาเริ่ม" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" data-mask="99:99" placeholder="เวลาสิ้นสุด" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="number" placeholder="ราคา" class="form-control"></div><div class="action-time-group"><a href="javascript:void(0)" class="remove-time"><i class="mdi mdi-minus-circle ripplelink"></i></a></div></div>'
+
+  var x = 1; //initlal text box count
+  $(AddButton).click(function(e){ //on add input button click
+      e.preventDefault();
+      if(x < MaxFields){ //max input box allowed
+          x++; //text box increment
+          $(Wrapper).append(Row); //add input box
+      }
+  });
+
+  //ยังเขียนวิธีลบไม่ได้
+  // $(Wrapper).on("click",".remove-time", function(e){ //user click on remove text
+  //     e.preventDefault();
+  //     $(this).parent('div').remove();
+  //     x--;
+  // })
 
   /*Field*/
   $('.add-cover a').bind('click', function() {
@@ -45,11 +65,5 @@ $(function(){
       $('.add-cover').fadeOut();
     }, 1000);
   }
-
-  // var Row = $('#fields-form .time-wraper .row');
-  // var TimeWrapper = $('#fields-form .time-wraper');
-  // $('.add-time-group a').click(function(){
-  //   $(TimeWraper)append(Row);
-  // });
 
 });
