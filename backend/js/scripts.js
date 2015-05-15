376084ea57d5ad54a469aaa127b343fd131f3dac
Champ <twinsynergy@Twinsynergys-MacBook-Pro.local>
2015-05-15 16:19:27 +0700

    validate add football field

--- backend/js/scripts.js
+++ backend/js/scripts.js
@@ -1,5 +1,6 @@
 $(function(){
-  var ink, d, x, y;
+    //click circle fade
+    var ink, d, x, y;
     $(".ripplelink").click(function(e){
       if($(this).find(".ink").length === 0){
         $(this).prepend("<span class='ink'></span>");
@@ -26,20 +27,21 @@ $(function(){
     $('body').toggleClass('menubar-visible')
   });
 
+  //counter nubmer animate
   $('.counter').counterUp({
     delay: 10,
     time: 1000
   });
 
+  //tooltip bootstrap
   $('[data-toggle="tooltip"]').tooltip();
 
   /*Form*/
   var MaxFields = 10;
-  var Wrapper = $('#fields-form .time-wraper');
-  var AddButton = $("#fields-form .insert-time");
-  var RemoveButton = $('#fields-form .remove-time');
-  var Row = '<div class="row time-row time-minus-row"><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" data-mask="99:99" placeholder="เวลาเริ่ม" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" data-mask="99:99" placeholder="เวลาสิ้นสุด" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="number" placeholder="ราคา" class="form-control"></div><a href="javascript:void(0)" class="action-time-group remove-time"><i class="mdi mdi-minus-circle ripplelink"></i></a></div>'
-
+  var Wrapper = $('#football-form .time-wraper');
+  var AddButton = $("#football-form .insert-time");
+  var RemoveButton = $('#football-form .remove-time');
+  var Row = '<div class="row time-row time-minus-row"><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" id="timestart_ff" name="timestart_ff" data-mask="99:99" placeholder="เวลาเริ่ม" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="text" id="timeend_ff" name="timeend_ff" data-mask="99:99" placeholder="เวลาสิ้นสุด" class="form-control"></div><div class="time-group col-xs-4 col-sm-4 col-md-4"><input type="number" placeholder="ราคา" id="price_ff" name="price_ff" class="form-control"></div><a href="javascript:void(0)" class="action-time-group remove-time"><i class="mdi mdi-minus-circle"></i></a></div>'
   var x = 1; //initlal text box count
   $(AddButton).click(function(e){ //on add input button click
       e.preventDefault();
@@ -48,8 +50,6 @@ $(function(){
           $(Wrapper).append(Row); //add input box
       }
   });
-
-  //ยังเขียนวิธีลบไม่ได้
   $(Wrapper).on("click",".remove-time", function(e){ //user click on remove text
       e.preventDefault();
       $(this).parent().remove();
@@ -65,5 +65,54 @@ $(function(){
       $('.add-cover').fadeOut();
     }, 1000);
   }
+
+  /*Validate
+  * ff == football fields
+  */
+  var errorText = "กรอกข้อมูล";
+  var numText = "ใส่ได้เฉพาะตัวเลข"
+
+  $('#football-form').validate({
+    errorClass: "help-block",
+    errorElement: "span",
+    rules:{
+      name_ff: "required",
+      detail_ff: "required",
+      weight_ff: "required",
+      timestart_ff: "required",
+      timeend_ff: "required",
+      team_ff:{
+        required: true,
+        number: true
+      },
+      price_ff:{
+        required: true,
+        number: true
+      } 
+    },
+    messages:{
+      name_ff: errorText,
+      detail_ff: errorText,
+      weight_ff: errorText,
+      timestart_ff: errorText,
+      timeend_ff: errorText,
+      team_ff:{
+        required: errorText,
+        number: numText
+      },
+      price_ff:{
+        required: errorText,
+        number: numText
+      }
+    },
+    highlight: function(element) {
+        $(element).parent('div').addClass('error');
+    },
+    unhighlight: function(element) {
+        $(element).parent('div').removeClass('error');
+    }
+  });
+  
+
 
 });
