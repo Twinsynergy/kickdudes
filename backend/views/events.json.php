b1d804dc18b7dde0753d401b5051fcd5062c4559
Champ <twinsynergy@Twinsynergys-MacBook-Pro.local>
2015-08-04 17:13:55 +0700

    change color event calendar
    booking page

--- backend/views/events.json.php
+++ backend/views/events.json.php
@@ -3,11 +3,12 @@
 	"result": [
 		{
 			"id": "293",
-			"title": "This is warning class event with very long title to check how it fits to evet in day view",
+			"title": "สนามเขียว",
 			"url": "http://www.example.com/",
-			"class": "event-warning",
+			"class": "event-red",
 			"start": "1362938400000",
-			"end":   "1363197686300"
+			"end":   "1363197686300",
+			"price": "2,000"
 		},
 		{
 			"id": "256",
